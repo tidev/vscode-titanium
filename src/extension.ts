24799ad192f54a54eb1708745418509dda5aa4b2
Josh Longton <longton95@gmail.com>
2019-04-25 13:56:56 +0100

    refactor(completions): changed generate completions to non blocking (#77)
    * refactor(completions): changed generate completions to non blocking
    
    Changed generate completions to be non blocking and fixed the messages to show the status of the
    generation
    
    Fixes #22
    
    * chore(extension): addressed comments
    
    * refactor: remove unnecessary resolve

--- src/extension.ts
+++ src/extension.ts
@@ -196,23 +196,25 @@ exports.deactivate = deactivate;  // eslint-disable-line no-undef
  * Initialise extension - fetch appc info
  */
 async function init () {
-	const isEnabled = await ExtensionContainer.context.globalState.get<boolean>(GlobalState.Enabled);
+	const isEnabled = ExtensionContainer.context.globalState.get<boolean>(GlobalState.Enabled);
 	if (isEnabled) {
-		vscode.window.withProgress({ location: vscode.ProgressLocation.Window, title: 'Reading Appcelerator environment ...' }, async progress => {
-			if (await ExtensionContainer.context.globalState.get('titanium:liveview')) {
-				await vscode.commands.executeCommand('setContext', 'titanium:liveview', true);
-			}
-			appc.getInfo(async (error, info) => {
-				if (error) {
-					vscode.window.showErrorMessage('Error fetching Appcelerator environment');
-					return;
+		vscode.window.withProgress({ location: vscode.ProgressLocation.Notification, title: 'Reading Appcelerator environment ...' }, async progress => {
+			return new Promise((resolve, reject) => {
+				if (ExtensionContainer.context.globalState.get('titanium:liveview')) {
+					vscode.commands.executeCommand('setContext', 'titanium:liveview', true);
 				}
-				if (project.isTitaniumApp) {
-					await generateCompletions({ progress });
-				}
-				// Call refresh incase the Titanium Explorer activity pane became active before info
-				await vscode.commands.executeCommand(Commands.RefreshExplorer);
-
+				appc.getInfo(error => {
+					if (error) {
+						vscode.window.showErrorMessage('Error fetching Appcelerator environment');
+						return reject();
+					}
+					if (project.isTitaniumApp) {
+						generateCompletions({ progress });
+					}
+					// Call refresh incase the Titanium Explorer activity pane became active before info
+					vscode.commands.executeCommand(Commands.RefreshExplorer);
+					resolve();
+				});
 			});
 		});
 	}
