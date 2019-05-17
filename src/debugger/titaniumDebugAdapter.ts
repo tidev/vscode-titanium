241f3bc40d1f948ba2293b46296607807ba9f769
Ewan Harris <ewanharris93@gmail.com>
2019-05-17 17:21:37 +0100

    feat(debugger): terminate build on debug cancel

--- src/debugger/titaniumDebugAdapter.ts
+++ src/debugger/titaniumDebugAdapter.ts
@@ -1,4 +1,9 @@
-import { ChromeDebugAdapter, IAttachRequestArgs, ILaunchRequestArgs } from 'vscode-chrome-debug-core';
+import { ChromeDebugAdapter, ChromeDebugSession, IAttachRequestArgs, ILaunchRequestArgs } from 'vscode-chrome-debug-core';
+import { Event } from 'vscode-debugadapter';
+import { DebugProtocol } from 'vscode-debugprotocol';
+import { MESSAGE_STRING, Request } from '../common/extensionProtocol';
+import { BuildAppOptions } from '../types/cli';
+import { LogLevel } from '../types/common';
 
 interface TitaniumLaunchRequestArgs extends ILaunchRequestArgs {
 	appRoot?: string;
@@ -11,6 +16,24 @@ export class TitaniumDebugAdapter extends ChromeDebugAdapter {
 
 	public launch (args: TitaniumLaunchRequestArgs): Promise<void> {
 		return;
+	}
+	public disconnect (args: DebugProtocol.DisconnectArguments) {
+		this.sendRequest('END', args);
+		super.disconnect(args);
+	}
+
+	private sendRequest (code, args) {
+		const request: Request = {
+			id: `request-${++this.idCount}`,
+			code,
+			args
+		};
+
+		return new Promise(resolve => {
+			this.activeRequests.set(request.id, resolve);
+
+			this.session.sendEvent(new Event(MESSAGE_STRING, request));
+		});
 	}
 
 }
