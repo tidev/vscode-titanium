7a44a7c9351b63657080c4f7ed4e170edd2e1008
longton95 <longton95@gmail.com>
2019-10-07 11:33:50 +0100

    fix(clean): added support for modules

--- src/commands/clean.ts
+++ src/commands/clean.ts
@@ -1,14 +1,23 @@
+import * as path from 'path';
 import { workspace } from 'vscode';
 import { ExtensionContainer } from '../container';
+import project from '../project';
+import { selectPlatform } from '../quickpicks/common';
 import { cleanAppArguments } from '../utils';
-import {  handleInteractionError, InteractionError } from './common';
+import { handleInteractionError, InteractionError } from './common';
 
 import { CleanAppOptions } from '../types/cli';
 
 export async function cleanApplication () {
 	try {
 		const logLevel = ExtensionContainer.config.general.logLevel;
-		const projectDir = workspace.rootPath;
+		let projectDir = workspace.rootPath;
+
+		if (project.isTitaniumModule) {
+			const platformInfo = await selectPlatform();
+			projectDir = path.join(projectDir, platformInfo.id);
+		}
+
 		const cleanArguments: CleanAppOptions = {
 			logLevel,
 			projectDir,
