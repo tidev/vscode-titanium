ae9b83c5b9c0d4a910f38863dbb14d2355be40fe
Ewan Harris <ewanharris93@gmail.com>
2020-04-16 16:25:19 +0100

    build: rough first pass at code coverage

--- src/test/index.ts
+++ src/test/index.ts
@@ -1,8 +1,13 @@
 import * as glob from 'glob';
 import * as Mocha from 'mocha';
 import * as path from 'path';
+import * as fs from 'fs-extra';
 
-export function run (): Promise<void> {
+// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+// @ts-ignore
+import NYC = require('nyc');
+
+export async function run (): Promise<void> {
 	// Create the mocha test
 	const reportPath = path.join(__dirname, '..', '..', 'junit_report.xml');
 
@@ -17,6 +22,14 @@ export function run (): Promise<void> {
 		}
 	});
 	mocha.useColors(true);
+
+	const nyc = new NYC({
+		exclude: '.vscode-test/**',
+		include: 'src/**',
+		reporter: 'html'
+	});
+
+	await nyc.createTempDirectory();
 
 	const testsRoot = path.resolve(__dirname, '..');
 
@@ -42,5 +55,18 @@ export function run (): Promise<void> {
 				return reject(err);
 			}
 		});
+	}).then(async () => {
+		const coverage = (global as any).__coverage__;
+		if (coverage) {
+			const coverageFolder = path.join('/Users/awam/git/editors/vscode-appcelerator-titanium/.nyc_output');
+			console.log(coverageFolder);
+			fs.ensureDirSync(coverageFolder);
+			const writeStream = fs.createWriteStream(
+				path.join(coverageFolder, 'coverage.json')
+			);
+			writeStream.write(JSON.stringify(coverage));
+			writeStream.end();
+			await nyc.report();
+		}
 	});
 }
