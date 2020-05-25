8279b452d3b2d3e8508b57a8a915f25f3c242a09
Ewan Harris <ewanharris93@gmail.com>
2020-05-25 16:23:44 +0100

    build(test): improve code coverage setup
    This change excludes the test from the code coverage and also allows us to track all js files in the
    extension rather than only those that were required in the tests

--- src/test/index.ts
+++ src/test/index.ts
@@ -1,14 +1,12 @@
 import * as glob from 'glob';
 import * as Mocha from 'mocha';
 import * as path from 'path';
-import * as fs from 'fs-extra';
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
 // @ts-ignore
 import NYC = require('nyc');
 
 export async function run (): Promise<void> {
-	// Create the mocha test
 	const reportPath = path.join(__dirname, '..', '..', 'junit_report.xml');
 
 	const mocha = new Mocha({
@@ -24,49 +22,41 @@ export async function run (): Promise<void> {
 	mocha.useColors(true);
 
 	const nyc = new NYC({
-		exclude: '.vscode-test/**',
-		include: 'src/**',
-		reporter: 'html'
+		cwd: path.join(__dirname, '..', '..'),
+		exclude: [ '.vscode-test/**', '**/test/**' ],
+		reporter: [ 'text', 'html' ],
+		all: true,
+		instrument: true,
+		hookRequire: true,
+		hookRunInContext: true,
+		hookRunInThisContext: true
 	});
+
+	nyc.reset();
+	nyc.wrap();
 
 	await nyc.createTempDirectory();
 
 	const testsRoot = path.resolve(__dirname, '..');
 
-	return new Promise((resolve, reject) => {
-		glob('**/**.test.js', { cwd: testsRoot }, (error, files) => {
-			if (error) {
-				return reject(error);
-			}
+	const files = glob.sync('**/**.test.js', { cwd: testsRoot });
 
-			// Add files to the test suite
-			files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
+	// Add files to the test suite
+	files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
 
-			try {
-				// Run the mocha test
-				mocha.run(failures => {
-					if (failures > 0) {
-						return reject(new Error(`${failures} tests failed.`));
-					} else {
-						return resolve();
-					}
-				});
-			} catch (err) {
-				return reject(err);
-			}
+	try {
+		// Run the mocha test
+		await new Promise((resolve, reject) => {
+			mocha.run(failures => {
+				if (failures > 0) {
+					return reject(new Error(`${failures} tests failed.`));
+				} else {
+					return resolve();
+				}
+			});
 		});
-	}).then(async () => {
-		const coverage = (global as any).__coverage__;
-		if (coverage) {
-			const coverageFolder = path.join('/Users/awam/git/editors/vscode-appcelerator-titanium/.nyc_output');
-			console.log(coverageFolder);
-			fs.ensureDirSync(coverageFolder);
-			const writeStream = fs.createWriteStream(
-				path.join(coverageFolder, 'coverage.json')
-			);
-			writeStream.write(JSON.stringify(coverage));
-			writeStream.end();
-			await nyc.report();
-		}
-	});
+	} finally {
+		nyc.writeCoverageFile();
+		nyc.report();
+	}
 }
