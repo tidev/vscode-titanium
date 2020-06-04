da408a1bf382429e462ceac8741e4015a65ce993
Ewan Harris <ewanharris93@gmail.com>
2020-06-04 15:06:09 +0100

    refactor(quickpicks): reduce lint warnings

--- src/quickpicks/creation.ts
+++ src/quickpicks/creation.ts
@@ -2,7 +2,7 @@ import { nameForPlatform, platforms } from '../utils';
 import { CustomQuickPick, quickPick } from './common';
 
 export async function selectPlatforms (): Promise<string[]> {
-	const choices: CustomQuickPick[] = platforms().map(platform => ({ label: nameForPlatform(platform)!, id: platform, picked: true }));
+	const choices: CustomQuickPick[] = platforms().map(platform => ({ label: nameForPlatform(platform), id: platform, picked: true }));
 	const selected = await quickPick(choices, { canPickMany: true, placeHolder: 'Choose platforms' });
 	return selected.map((platform: CustomQuickPick)  => platform.id);
 }
