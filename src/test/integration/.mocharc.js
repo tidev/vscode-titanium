8aa7f837510464e931be26e0d294af304ca1fcef
Ewan Harris <ewanharris93@gmail.com>
2020-06-16 17:43:24 +0100

    test: setup junit output

---
+++ src/test/integration/.mocharc.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const reportPath = path.join(__dirname, '..', '..', '..', 'junit_report-ui.xml');
+
+module.exports = {
+	reporter: 'mocha-multi-reporters',
+		reporterOptions: {
+			reporterEnabled: 'mocha-jenkins-reporter, spec',
+			mochaJenkinsReporterReporterOptions: {
+				junit_report_path: reportPath // eslint-disable-line @typescript-eslint/camelcase
+			}
+		}
+};
