109bcbd6c71e2363706573557d66370a418a1b09
Ewan Harris <ewanharris93@gmail.com>
2020-06-16 16:57:47 +0100

    refactor: reduce duplicate code in test suites

--- src/test/integration/runTests.ts
+++ src/test/integration/runTests.ts
@@ -1,14 +1,19 @@
 import { ExTester } from 'vscode-extension-tester';
 import * as tmp from 'tmp';
 import * as fs from 'fs-extra';
+import { testSetup } from './util/common';
 
-// We create a temporary directory for the "Extension directory" so that only our extension and
-// the base vscode extensions get loaded
-const tempDirectory = tmp.dirSync();
-try {
-	const tester = new ExTester(undefined, undefined, tempDirectory.name);
-	tester.setupAndRunTests(undefined, 'out/test/integration/suite/**/*.test.js');
-} finally {
-	fs.removeSync(tempDirectory.name);
+async function main (): Promise<void> {
+	// We create a temporary directory for the "Extension directory" so that only our extension and
+	// the base vscode extensions get loaded
+	const tempDirectory = tmp.dirSync();
+	try {
+		await testSetup();
+		const tester = new ExTester(undefined, undefined, tempDirectory.name);
+		tester.setupAndRunTests(undefined, 'out/test/integration/suite/**/*.test.js');
+	} finally {
+		fs.removeSync(tempDirectory.name);
+	}
 }
 
+main();
--- src/test/integration/suite/create-app.test.ts
+++ src/test/integration/suite/create-app.test.ts
@@ -1,69 +1,17 @@
 import { expect } from 'chai';
-import { EditorView, VSBrowser, Workbench, WebDriver, InputBox } from 'vscode-extension-tester';
+import { EditorView, VSBrowser, WebDriver, Workbench } from 'vscode-extension-tester';
 import * as path from 'path';
-import { DialogHandler } from 'vscode-extension-tester-native';
 import * as fs from 'fs-extra';
 import * as tmp from 'tmp';
 import * as xml2js from 'xml2js';
-import { notificationExists, parsePlatformsFromTiapp } from '../util/common';
-
-/**
- * Creates an application project
- *
- * @param {CreateOptions} createOptions - Properties required for creation of the project
- * @param {WebDriver} driver - WebDriver instance
- * @param {WorkBench} wb - Workbench instance
- */
-async function createApplication ({ id, enableServices, folder, name, platforms }: AppCreateOptions, driver: WebDriver, wb: Workbench): Promise<void> {
-	await wb.executeCommand('Titanium: Create Titanium application');
-	const input = await InputBox.create();
-	// Input name
-	await input.setText(name);
-	await input.confirm();
-	// Input app ID
-	await input.setText(id);
-	await input.confirm();
-	await driver.sleep(100);
-	// Select platforms
-	const choices = await input.getQuickPicks();
-	for (const choice of choices) {
-		const text = await choice.getText();
-		if (!platforms.includes(text.toLowerCase())) {
-			await input.selectQuickPick(text);
-			await driver.sleep(50);
-		}
-	}
-	await input.confirm();
-
-	// No services
-	const servicesText = enableServices ? 'Yes' : 'No';
-	await input.setText(servicesText);
-	await input.confirm();
-	await driver.sleep(100);
-
-	// Browse for file location
-	await input.confirm();
-	const dialog = await DialogHandler.getOpenDialog();
-	await dialog.selectPath(folder);
-	await dialog.confirm();
-
-	await driver.wait(() => notificationExists('Creating application'), 1000);
-
-	await driver.wait(async () => {
-		// We need to sleep here as there are times when the "Creating application" notification
-		// is still shown but is dismissed by the time we get the text in notificationExists and
-		// causes errors to be thrown that can't be handled
-		await driver.sleep(500);
-		return notificationExists('Project created');
-	}, 25000);
-}
+import { parsePlatformsFromTiapp, dismissNotifications } from '../util/common';
+import { ProjectCreator } from '../util/create';
 
 describe('Application creation', function () {
 	this.timeout(30000);
 
 	let browser: VSBrowser;
 	let driver: WebDriver;
-	let wb: Workbench;
 	let tempDirectory: tmp.DirResult;
 
 	beforeEach(async function () {
@@ -72,8 +20,8 @@ describe('Application creation', function () {
 		const editorView = new EditorView();
 		await editorView.closeAllEditors();
 		await browser.waitForWorkbench();
-		wb = new Workbench();
 		tempDirectory = tmp.dirSync();
+		await dismissNotifications();
 	});
 
 	afterEach(async function () {
@@ -83,13 +31,15 @@ describe('Application creation', function () {
 	it('should be able to create a project', async function () {
 		this.timeout(60000);
 		const name = 'vscode-e2e-test-app';
-		await createApplication({
+		const creator = new ProjectCreator(driver);
+
+		await creator.createApp({
 			id: 'com.axway.e2e',
 			enableServices: false,
 			folder: tempDirectory.name,
 			name,
 			platforms: [ 'android', 'ios' ]
-		}, driver, wb);
+		});
 
 		const projectPath = path.join(tempDirectory.name, name);
 		const tiappPath = path.join(projectPath, 'tiapp.xml');
@@ -105,13 +55,14 @@ describe('Application creation', function () {
 	it('should only enable selected platforms', async function () {
 		this.timeout(60000);
 		const name = 'vscode-e2e-test-app';
-		await createApplication({
+		const creator = new ProjectCreator(driver);
+		await creator.createApp({
 			id: 'com.axway.e2e',
 			enableServices: false,
 			folder: tempDirectory.name,
 			name,
 			platforms: [ 'android' ]
-		}, driver, wb);
+		});
 
 		const projectPath = path.join(tempDirectory.name, name);
 		const tiappPath = path.join(projectPath, 'tiapp.xml');
