68a8f874afe1a2e53c3ed62f321ff1083ee54437
Ewan Harris <ewanharris93@gmail.com>
2021-03-01 10:39:53 +0000

    test: refactor test setup code to new api

--- src/test/integration/runTests.ts
+++ src/test/integration/runTests.ts
@@ -1,7 +1,7 @@
 import * as fs from 'fs-extra';
 import * as path from 'path';
 import * as tmp from 'tmp';
-import { ExTester } from 'vscode-extension-tester';
+import { ExTester, ReleaseQuality } from 'vscode-extension-tester';
 import { testSetup } from './util/common';
 
 async function main (): Promise<void> {
@@ -10,9 +10,9 @@ async function main (): Promise<void> {
 	const tempDirectory = tmp.dirSync();
 	try {
 		await testSetup();
-		const tester = new ExTester(undefined, undefined, tempDirectory.name);
+		const tester = new ExTester(undefined, ReleaseQuality.Stable, tempDirectory.name);
 		const mochaConfig = path.join(__dirname, '.mocharc.js');
-		tester.setupAndRunTests(undefined, 'out/test/integration/suite/**/*.test.js', undefined, false, true, mochaConfig);
+		tester.setupAndRunTests('out/test/integration/suite/**/*.test.js', undefined, undefined, { config: mochaConfig });
 	} finally {
 		fs.removeSync(tempDirectory.name);
 	}
