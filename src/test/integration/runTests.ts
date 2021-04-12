752950e5851a9f8d8d56b8f0375499fbb4100aaa
Ewan Harris <ewanharris93@gmail.com>
2021-04-12 15:26:26 +0100

    test: set useTi to true for integration tests

--- src/test/integration/runTests.ts
+++ src/test/integration/runTests.ts
@@ -2,7 +2,7 @@ import * as fs from 'fs-extra';
 import * as path from 'path';
 import * as tmp from 'tmp';
 import { ExTester, ReleaseQuality } from 'vscode-extension-tester';
-import { testSetup } from './util/common';
+import { getFixturesDirectory, testSetup } from './util/common';
 
 async function main (): Promise<void> {
 	// We create a temporary directory for the "Extension directory" so that only our extension and
@@ -12,7 +12,8 @@ async function main (): Promise<void> {
 		await testSetup();
 		const tester = new ExTester(undefined, ReleaseQuality.Stable, tempDirectory.name);
 		const mochaConfig = path.join(__dirname, '.mocharc.js');
-		tester.setupAndRunTests('out/test/integration/suite/**/*.test.js', undefined, undefined, { config: mochaConfig });
+		const settings = path.join(getFixturesDirectory(), 'settings.json');
+		tester.setupAndRunTests('out/test/integration/suite/**/*.test.js', undefined, undefined, { config: mochaConfig, settings });
 	} finally {
 		fs.removeSync(tempDirectory.name);
 	}
