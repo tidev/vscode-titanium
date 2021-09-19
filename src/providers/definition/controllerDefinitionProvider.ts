db8ee8f212fbdcc22fe2939f0c972e5a8a35a695
Jason Kneen <jason.kneen@bouncingfish.com>
2021-09-19 12:50:25 +0100

    added support for other import parameters like "as", * etc.

--- src/providers/definition/controllerDefinitionProvider.ts
+++ src/providers/definition/controllerDefinitionProvider.ts
@@ -13,7 +13,7 @@ export class ControllerDefinitionProvider extends BaseDefinitionProvider {
 			}
 		},
 		{ // ES6 import from (/lib) name
-			regExp: /import (?:[-a-zA-Z0-9-_/\s]*)['"]([-a-zA-Z0-9-_/]*)$/,
+			regExp: /import (?:[-a-zA-Z0-9-_/\[\]\*\,\s]*)['"]([-a-zA-Z0-9-_/]*)$/,
 			files (project: Project, document: vscode.TextDocument, text: string, value: string): string[] {
 				return [path.join(project.filePath, 'app', 'lib', `${value}.js`)];
 			}
