2fbac6ecce903c97c736a256043c3a33fae5861f
Jason Kneen <jason.kneen@bouncingfish.com>
2021-09-19 12:30:56 +0100

    added ES6 import file lookups
    Along with ES5 `require` this should add support to CMD+Click:
    
    ```
    import navigator from "core/navigator";
    
    import "core/navigator";
    ```
    
    to open the related file.

--- src/providers/definition/controllerDefinitionProvider.ts
+++ src/providers/definition/controllerDefinitionProvider.ts
@@ -12,6 +12,12 @@ export class ControllerDefinitionProvider extends BaseDefinitionProvider {
 				return [ path.join(project.filePath, 'app', 'lib', `${value}.js`) ];
 			}
 		},
+		{ // ES6 import from (/lib) name
+                	regExp: /import (?:[-a-zA-Z0-9-_/\s]*)['"]([-a-zA-Z0-9-_/]*)$/,
+	                files(project, document, text, value) {
+	                    return [path.join(project.filePath, 'app', 'lib', `${value}.js`)];
+        	        }
+            	},
 		{ // controller name
 			regExp: /Alloy\.createController\(["']([-a-zA-Z0-9-_/]*)$/,
 			files (project: Project, document: vscode.TextDocument, text: string, value: string): string[] {
