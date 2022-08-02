75599eae830d4252e34ef65c1c03df4e7d6d5911
Ewan Harris <ewanharris93@gmail.com>
2022-08-02 22:31:26 +0100

    build: setup semantic release publishing

---
+++ release.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+	plugins: [
+		'@semantic-release/commit-analyzer',
+		'@semantic-release/release-notes-generator',
+		'@semantic-release/changelog',
+		[
+			'semantic-release-vsce',
+			{
+				packageVsix: true,
+			},
+		],
+		'@semantic-release/git',
+		[
+			'@semantic-release/github',
+			{
+				assets: '*.vsix',
+				addReleases: 'bottom',
+				successComment: false
+			}
+		]
+	]
+};
