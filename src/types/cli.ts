42a7c99ceac1c0f8629c6d99d3b97bd349fa1d45
Ewan Harris <ewanharris93@gmail.com>
2023-04-27 22:11:03 +0100

    feat: support building for macos

--- src/types/cli.ts
+++ src/types/cli.ts
@@ -18,9 +18,9 @@ export interface CreateModuleOptions extends CreateOptions {
 }
 
 export type Target = DevelopmentTarget | DeploymentTarget;
-export type DevelopmentTarget = 'device' | 'emulator' | 'simulator';
-export type DeploymentTarget = 'dist-adhoc' | 'dist-appstore' | 'dist-playstore';
+export type DevelopmentTarget = 'device' | 'emulator' | 'macos' | 'simulator';
+export type DeploymentTarget = 'dist-adhoc' | 'dist-appstore' | 'dist-macappstore' | 'dist-playstore';
 
 export type PrettyTarget = PrettyDevelopmentTarget | PrettyDeploymentTarget;
-export type PrettyDevelopmentTarget = 'Device' | 'Emulator' | 'Simulator';
-export type PrettyDeploymentTarget = 'Ad-Hoc' | 'App Store' | 'Play Store';
+export type PrettyDevelopmentTarget = 'Device' | 'Emulator' | 'Simulator' | 'Mac OS';
+export type PrettyDeploymentTarget = 'Ad-Hoc' | 'App Store' | 'MacOS App Store' | 'Play Store';
